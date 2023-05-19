045ab86700e34b3c787e511b22ae213cf23b88d9
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-05-19 13:26:45 -0300

    Add files via upload

---
+++ PastaPHP/listarDados/listarCurso.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once "classes/Curso.php";
+$curso = new Curso();
+$listaCurso = $curso -> listar();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ListarCurso</title>
+</head>
+<body>
+    <h2>Cursos</h2>
+    <table border="1">
+        <tr>
+        <th>id_curso</th>
+        <th>Nome_Curso</th>      
+        <th>sigla_curso</th>      
+     
+      
+        <th>Ações</th>
+       
+
+        </tr>
+        <td><?php foreach ($listaCurso as $linha): ?>
+            <tr>
+                <td><?php echo $linha['id_curso'] ?></td>
+                <td> <?php echo $linha ['Nome_Curso']?></td>
+                <td> <?php echo $linha ['sigla_curso']?></td>
+             <td>
+                <a href="#">Atualizar</a>
+                <a href="#">Excluir</a>
+
+        </td>
+        </tr>
+        <?php endforeach ?>
+        </table>
+        <a href="cadastro Edital.html" >Novo_Edital</a>
+        <a href="index2.php" >Voltar</a>
+</body>
+</html>
