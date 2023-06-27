5f61e0c4768eef09c274fbe8a1ec75e2812652a4
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-06-27 09:38:08 -0300

    Add files via upload

---
+++ PHP/Classes/Status/listarStatus.php
@@ -0,0 +1,129 @@
+<?php
+
+require_once "classes/Status.php";
+$status = new Status();
+$listaStatus = $status -> listar();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Status</title>
+</head>
+<body>
+    <div id="topo">
+        <div class="cabeçario">
+            <img src="img/sp2.png" class="cab1">
+        </div>
+
+        <div class="cabeçario">
+            <a href="index.php"><img src="img/fatec2.png" class="cab2"></a>
+        </div>
+
+        <div class="cabeçario">
+            <img src="img/itapira2.png" class="cab3">
+        </div>
+    </div>
+
+    <div class="separacao"></div>
+
+    <div>
+        <img src="img/fatec.jpg" class="titulo">
+    </div>
+
+    <a href="dsm.html">
+        <div class="curso">
+            <p>Desenvolvimento de Software Multiplataforma</p>
+        </div>
+    </a>
+
+    <a href="gpi.html">
+        <div class="curso">
+            <p>Gestão da Produção Industrial</p>
+        </div>
+    </a>
+
+    <a href="ge.html">
+        <div class="curso">
+            <p>Gestão Empresarial</p>
+        </div>
+    </a>
+
+    <a href="gti.html">
+        <div class="curso">
+            <p>Gestão da Tecnologia da Informação</p>
+        </div>
+    </a>
+
+    <a href="login.html">
+        <div class="curso">
+            <p>Login</p>
+        </div>
+    </a>
+    <h2>Horario</h2>
+    <table border="1">
+        <tr>
+            <th>ID Status</th>
+            <th>Status</th>  
+            <th>Ações</th>
+        </tr>
+        <?php foreach ( $listaStatus as $linha): ?>
+            <tr>
+                <td><?php echo $linha['id'] ?></td>
+                <td><?php echo $linha['status']?></td>
+                <td>
+                    <a href="horario-excluir.php?id_status=<?= $linha['id'] ?>">Excluir</a>
+                    <a href="horario-editar.php?id_status=<?= $linha['id'] ?>">Atualizar</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+        </table>
+        <a href="cadastro Edital.php">Novo Edital</a>
+        <a href="index2.php">Voltar</a>
+
+           <div class="rodape">
+
+        <div class="fatec">
+
+            <h1 class="detalhe"><b>____</b></h1>
+
+            <h1>Fatec Itapira</h1>
+
+            <br>
+
+            <b>Endereço:</b> Rua Tereza Lera Paoletti, 570/590 - Jardim Bela Vista
+
+            <br>
+
+            <b>CEP:</b> 13.974-080
+
+            <br>
+
+            <b>Telefones:</b> (19) 3843-1996
+
+            <br>
+
+            <b>WhatsApp:</b> (19) 98933-6291 | (19) 3863-5210  
+
+            <br>
+
+            <b>E-mail:</b> contato@fatecitapira.edu.br
+        </div>
+
+        <div class="centro"></div>
+
+        <div class="sp">
+            <img src="img/saopaulo.png" class="imagem">
+        </div>   
+
+   </div>
+
+   <div class="direito">
+       <p id="magin">Todos os direitos reservados</p>
+   </div>
+</body>
+</html>
