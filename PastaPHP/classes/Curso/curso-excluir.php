3e085fc772739fe7ff08137292fed7f69d52699e
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2023-06-26 23:12:19 -0300

    Delete PastaPHP/classes directory

--- PastaPHP/classes/Curso/curso-excluir.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'classes/Curso.php';
-$id_curso = $_GET['id_curso'];
-$curso = new Curso($id_curso);
-$curso->excluir();
-header('Location: listarCurso.php');
-?>
--- PastaPHP/classes/Horario/horario-excluir.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'classes/Horario.php';
-$id_horario= $_GET['id_horario'];
-$horario = new Horario($id_horario);
-$horario->excluir();
-header('Location: listarHorario.php');
-?>
